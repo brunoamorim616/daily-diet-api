f0284cf2948fca7ff7cf95911ffdf90d9b81d9f2
bruno <amorimbq@gmail.com>
2023-09-09 18:21:04 -0300

    feat: created users and meals migrations

--- db/migrations/20230909195833_create-meal.ts
+++ db/migrations/20230909195833_create-meal.ts
@@ -2,15 +2,17 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('transactions', (table) => {
+  await knex.schema.createTable('meals', (table) => {
     table.uuid('id').primary().index().notNullable()
-    table.text('title').notNullable()
+    table.text('name').notNullable()
     table.text('description').notNullable()
-    table.decimal('amount', 10, 2).notNullable()
+    table.boolean('in_diet').notNullable()
+    table.timestamp('eaten_at').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.uuid('user').references('id').inTable('users').onDelete('CASCADE')
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('transactions')
+  await knex.schema.dropTable('meals')
 }
